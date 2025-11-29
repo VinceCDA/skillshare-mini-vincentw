a6c11cedf53dc7a05901c81089e61ec5c79ac509
VinceCDA <84965037+VinceCDA@users.noreply.github.com>
2025-11-29 13:30:52 +0100

    added api call to weather api

--- src/Controller/DashBoardController.php
+++ src/Controller/DashBoardController.php
@@ -2,15 +2,19 @@
 
 namespace App\Controller;
 
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class DashBoardController extends AbstractController
 {
-    #[Route('/dash/board', name: 'app_dash_board')]
-    public function index(): Response
+    #[Route('/dashboard', name: 'app_dash_board')]
+    public function index(HttpClientInterface $client): Response
     {
+        $uri = sprintf('https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=%s&units=metric&lang=fr',  $this->getParameter('app.apikeyweather'));
+        $response = $client->request('GET', $uri);
+        dd($response->getContent());
         return $this->render('dash_board/index.html.twig', [
             'controller_name' => 'DashBoardController',
         ]);
