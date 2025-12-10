d9d0374e97ebe01f98f10c79a5f7f13bc85534cb
VinceCDA <84965037+VinceCDA@users.noreply.github.com>
2025-12-10 16:53:33 +0100

    updated compose files

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
  
-composer install -n
+
 php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
 bin/console doc:fix:load --no-interaction
 exec "$@"
